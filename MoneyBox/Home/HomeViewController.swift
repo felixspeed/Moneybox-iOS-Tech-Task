fe556f2ce35dd6fa2ca1fe24604018c177529102
Felix Speed <felixbspeed@gmail.com>
2024-09-14 13:59:03 +0100

    Add title to test home screen

--- MoneyBox/Home/HomeViewController.swift
+++ MoneyBox/Home/HomeViewController.swift
@@ -8,7 +8,31 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .primaryBackground
         
-//        setupView()
-//        setupLayout()
+        setupView()
+        setupLayout()
+    }
+    
+    private var homeTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Test Home"
+        label.textColor = .accent
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+}
+
+extension HomeViewController {
+    func setupView() {
+        view.addSubview(homeTitle)
+    }
+}
+
+extension HomeViewController {
+    func setupLayout() {
+        NSLayoutConstraint.activate([
+            homeTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            homeTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        ])
     }
 }
